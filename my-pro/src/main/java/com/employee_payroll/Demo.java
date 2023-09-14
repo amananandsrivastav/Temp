6b356ddfbc6ede751c0ab2fd8bdd11e9a1d4470c
Aman Anand Srivastav <amananandsri2001@gmail.com>
2023-09-14 23:51:37 +0530

    learning commit

---
+++ my-pro/src/main/java/com/employee_payroll/Demo.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.employee_payroll;
+
+import java.awt.image.ColorModel;
+import java.awt.image.ImageConsumer;
+import java.util.Hashtable;
+
+/**
+ * 
+ */
+public class Demo implements ImageConsumer {
+
+	/**
+	 * 
+	 */
+	public Demo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setDimensions(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setProperties(Hashtable<?, ?> props) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setColorModel(ColorModel model) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setHints(int hintflags) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPixels(int x, int y, int w, int h, ColorModel model, byte[] pixels, int off, int scansize) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPixels(int x, int y, int w, int h, ColorModel model, int[] pixels, int off, int scansize) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void imageComplete(int status) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ my-pro/src/main/java/com/employee_payroll/MyProApplication.java
@@ -0,0 +1,13 @@
+package com.employee_payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProApplication.class, args);
+	}
+
+}
